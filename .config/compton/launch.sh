f04a5f5dac0b1e5aef689acd69e1b23c5887a986
Asael Jaimes <asaelx@gmail.com>
2021-06-22 05:23:53 -0500

    Add new and updated scripts & configs

--- .config/compton/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q compton
-
-compton -b --config ~/.config/compton/compton.conf
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,6 +8,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-polybar hdmi &
-polybar dvi &
+# Launch bar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top-main &
+  done
+else
+  polybar -c ~/.config/polybar/config.ini top-main &
+fi
